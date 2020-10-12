f4476db5347b9842fa43a832d749fc0a768b188a
Scoticus <scoticus@users.noreply.github.com>
2020-10-12 14:43:20 +0100

    Add FaunaDB query for last draw number

--- functions/getLottoResult.js
+++ functions/getLottoResult.js
@@ -1,5 +1,13 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const faunadb = require('faunadb');
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+  keepAlive: false,
+});
+
 exports.handler = async (event, context) => {
+  // Check request includes correct auth secret
   const reqBody = JSON.parse(event.body);
   if (reqBody.auth !== process.env.TRIGGER_SECRET) {
     console.log('Incorrect auth');
@@ -8,4 +16,15 @@ exports.handler = async (event, context) => {
     };
   }
   console.log('Correct auth');
+
+  // Get the previous draw number from DB
+  const getLastDraw = await client.query(
+    q.Get(q.Match(q.Index('last_lotto_draw_no')))
+  );
+  const lastDrawNo = getLastDraw.data.drawNumber;
+  const newDrawNo = parseInt(lastDrawNo) + 1;
+  const queryUrl =
+    'https://www.national-lottery.co.uk/results/lotto/draw-history/draw-details/' +
+    newDrawNo;
+  console.log({ queryUrl });
 };
