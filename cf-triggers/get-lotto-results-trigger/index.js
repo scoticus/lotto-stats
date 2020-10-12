1cf2bc3ddc2d6b023885cf4ebf335541d8c52f19
Scoticus <scoticus@users.noreply.github.com>
2020-10-12 13:47:40 +0100

    Add trigger script for getLottoResults

---
+++ cf-triggers/get-lotto-results-trigger/index.js
@@ -0,0 +1,20 @@
+addEventListener('scheduled', (event) => {
+  event.waitUntil(handleSchedule(event.scheduledTime));
+});
+
+async function handleSchedule() {
+  const url = LOTTO_ENDPOINT;
+  const authBody = {
+    auth: TRIGGER_SECRET,
+  };
+
+  let trigger = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(authBody),
+  });
+  console.log(trigger.status);
+  return new Response('Get lotto result', { status: 200 });
+}
