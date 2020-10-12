3d2e686854ae40e54c3602c548dbdd279892638f
Scoticus <scoticus@users.noreply.github.com>
2020-10-12 15:44:46 +0100

    Add parse latest results page and shape new data

--- functions/getLottoResult.js
+++ functions/getLottoResult.js
@@ -1,6 +1,7 @@
 const faunadb = require('faunadb');
 const q = faunadb.query;
 const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
@@ -35,5 +36,47 @@ exports.handler = async (event, context) => {
   );
   const latestResultsPage = await pageContent.text();
 
-  console.log(latestResultsPage);
+  // Parse page content and shape data
+  const $ = cheerio.load(latestResultsPage);
+
+  function findSimpleData(selector) {
+    return $(selector).text().trim().split(' ').pop();
+  }
+
+  const drawStr = findSimpleData('#header_draw_number');
+  const jackpotStr = findSimpleData('#game_header_intro');
+  const machine = findSimpleData('#header_draw_machine');
+  const ballSetStr = findSimpleData('#header_ball_set');
+
+  const drawNumber = parseFloat(drawStr);
+  const jackpot = parseFloat(jackpotStr.slice(1).replace(/,/g, ''));
+  const ballSet = parseFloat(ballSetStr);
+
+  const dateStr = $('#section_header').children('h1').text().trim().slice(4);
+  const dateObj = new Date(dateStr);
+  const date = dateObj.toISOString();
+
+  const numberList = $('.draw_numbers_list')
+    .children('li')
+    .map(function (i, el) {
+      return $(this).text();
+    })
+    .get()
+    .join(' ');
+  const splitNumbers = numberList.split(' ');
+  const numbers = [];
+  for (i = 0; i < 6; i++) {
+    numbers.push(parseFloat(splitNumbers[i]));
+  }
+  const bonusNumber = parseFloat(splitNumbers.pop());
+
+  const newResult = {
+    drawNumber,
+    date,
+    jackpot,
+    machine,
+    ballSet,
+    numbers,
+    bonusNumber,
+  };
 };
