b2d5c481ea9945c1be14ac0b60ecbfb1fe19b369
Scoticus <scoticus@users.noreply.github.com>
2020-10-15 23:51:18 +0100

    Add fetch for latest game results to site homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,36 @@
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-faunadb`,
+      options: {
+        secret: `fnAD4NrLNXACB1nA9QbusJV0UnT5zgsfN4H74eGr`,
+        index: `all_thunderball`,
+        type: 'thunderball',
+      },
+    },
+    {
+      resolve: `gatsby-source-faunadb`,
+      options: {
+        secret: `fnAD4NrLNXACB1nA9QbusJV0UnT5zgsfN4H74eGr`,
+        index: `all_lotto`,
+        type: 'lotto',
+      },
+    },
+    {
+      resolve: `gatsby-source-faunadb`,
+      options: {
+        secret: `fnAD4NrLNXACB1nA9QbusJV0UnT5zgsfN4H74eGr`,
+        index: `all_euromillions`,
+        type: 'euromillions',
+      },
+    },
+    {
+      resolve: `gatsby-source-faunadb`,
+      options: {
+        secret: `fnAD4NrLNXACB1nA9QbusJV0UnT5zgsfN4H74eGr`,
+        index: `all_setforlife`,
+        type: 'setForLife',
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,115 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../../components/layout';
 
-export default function Home() {
+export default function Home({ data }) {
+  const lottoResult = data.allLotto.nodes[0];
+  const thunderballResult = data.allThunderball.nodes[0];
+  const euromillionsResult = data.allEuromillions.nodes[0];
+  const setForLifeResult = data.allSetForLife.nodes[0];
   return (
     <Layout>
       <h1>Welcome to LottoStats</h1>
+      <h2>Latest Results</h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Game</td>
+            <td>Draw</td>
+            <td>Date</td>
+            <td>Jackpot</td>
+            <td>Numbers</td>
+            <td>Bonus</td>
+          </tr>
+        </thead>
+        <tbody>
+          <TableGameRow gameResult={lottoResult} />
+          <TableGameRow gameResult={thunderballResult} />
+          <TableGameRow gameResult={euromillionsResult} />
+          <TableGameRow gameResult={setForLifeResult} />
+        </tbody>
+      </table>
     </Layout>
   );
 }
+
+function TableGameRow({ gameResult }) {
+  return (
+    <tr>
+      <td>{gameResult.internal.type}</td>
+      <td>{gameResult.drawNumber}</td>
+      <td>{gameResult.date}</td>
+      <td>{gameResult.jackpot}</td>
+      <td>
+        <ul>
+          {gameResult.numbers.map((number) => (
+            <li key={number}>{number}</li>
+          ))}
+        </ul>
+      </td>
+      <td>
+        {gameResult.bonusNumber || (
+          <ul>
+            {gameResult.bonusNumbers.map((number) => (
+              <li key={number}>{number}</li>
+            ))}
+          </ul>
+        )}
+      </td>
+    </tr>
+  );
+}
+
+export const query = graphql`
+  {
+    allThunderball(sort: { order: DESC, fields: drawNumber }, limit: 1) {
+      nodes {
+        internal {
+          type
+        }
+        drawNumber
+        date
+        jackpot
+        numbers
+        bonusNumber
+      }
+    }
+    allLotto(sort: { order: DESC, fields: drawNumber }, limit: 1) {
+      nodes {
+        internal {
+          type
+        }
+        drawNumber
+        date
+        jackpot
+        numbers
+        bonusNumber
+      }
+    }
+    allSetForLife(sort: { order: DESC, fields: drawNumber }, limit: 1) {
+      nodes {
+        internal {
+          type
+        }
+        drawNumber
+        date
+        jackpot
+        numbers
+        bonusNumber
+      }
+    }
+    allEuromillions(sort: { order: DESC, fields: drawNumber }, limit: 1) {
+      nodes {
+        internal {
+          type
+        }
+        drawNumber
+        date
+        jackpot
+        numbers
+        bonusNumbers
+      }
+    }
+  }
+`;
